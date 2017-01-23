56f11d0be67b061dd72ae26f2b5ac717acbd2cac
Mike <megamike25@gmail.com>
2017-01-22 19:12:44 -0600

    testing with imagestore pain

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,12 +5,12 @@ public class Player {
     private Group cards;
     private int foldedCorners = 0;
 
-    public Player(int x, int y, double rotate, ){
+    public Player(int x, int y, double rotate,Card card1, Card card2, Card card3, Card card4 ){
         cards = new Group();
         cards.getChildren().clear();
 
         for(int i = 0; i < 4; i++){
-            cards.getChildren().add(card);
+            //cards.getChildren().add(card);
         }
     }
 
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,11 +1,18 @@
+import javafx.scene.image.Image;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
 import static org.junit.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 public class PlayerTest {
 
     @Test
     public void foldACorner(){
-        Player player = new Player(0,0,0);
+        Player player = new Player(0,0,0, new Card(1,1), new Card(1,1), new Card(1,1), new Card(1,1));
         player.foldCorner();
 
         assertEquals(1, player.getFoldedCorners());
@@ -13,11 +20,14 @@ public class PlayerTest {
 
     @Test
     public void playerCreationSetsDisplayValuesCorrectly0DegRotation(){
-        Player player = new Player(200, 300, 0);
+        Player player = new Player(200, 300, 0, new Card(1,1), new Card(1,1), new Card(1,1), new Card(1,1));
 
         for(int i = 0; i < 4; i++){
             Card card = (Card)player.getCards().getChildren().get(i);
+
             assertEquals(300, card.get_card_position_y(),0);
+            assertEquals(200 + (Card.CARD_WIDTH + 20) * i, 0);
+            assertEquals(0, card.getRotate(), 0);
         }
 
     }
